b35e96625e0cd80c1960b86ba21363b12330de18
吳元皓 <hw@yuanhau.com>
2025-05-11 09:57:56 +0800

    feat: add news organization about page with dynamic content fetching and animation

--- scraping/main.py
+++ scraping/main.py
@@ -11,14 +11,14 @@ import os
 # Load environment variables from .env file
 dotenv.load_dotenv()
 
-# Connect to PostgresDB
-conn = psycopg2.connect(database=os.getenv("POSTGRES_DB"), 
-                        user=os.getenv("POSTGRES_USER"),
-                        password=os.getenv("POSTGRES_PASSWORD"),
-                        host=os.getenv("POSTGRES_HOST"),
-                        port=os.getenv("POSTGRES_PORT")
-)
 
+# Connect to PostgresDB
+#conn = psycopg2.connect(database=os.getenv("POSTGRES_DB"), 
+#                        user=os.getenv("POSTGRES_USER"),
+#                        password=os.getenv("POSTGRES_PASSWORD"),
+#                        host=os.getenv("POSTGRES_HOST"),
+#                        port=os.getenv("POSTGRES_PORT")
+#)
 
 headers = {
     'User-Agent': 'NewsSceraperBot/1.0 (https://github.com/hpware/news-analyze)'
@@ -28,35 +28,44 @@ headers = {
 #url = "https://news.google.com/home?hl=zh-TW&gl=TW&ceid=TW:zh-Hant"
 url = "https://news.google.com/topics/CAAqJQgKIh9DQkFTRVFvSUwyMHZNRFptTXpJU0JYcG9MVlJYS0FBUAE?hl=zh-TW&gl=TW&ceid=TW%3Azh-Hant"
 
+topiccwiz_css = "PO9Zff Ccj79 kUVvS"
 page = urlopen(url)
 html_bytes = page.read()
 html = html_bytes.decode("utf-8")
 soup = BeautifulSoup(html, "html.parser")
-hotarticles = soup.find_all("article")
+# Find the topiccwiz element
+topiccwiz = soup.find_all("c-wiz", class_=topiccwiz_css)
 news_data = []
-for article in hotarticles:
-    try:
-        title_elem = article.find('a', class_='gPFEn')
-        title = title_elem.text.strip() if title_elem else ''
-        
-        source_elem = article.find('div', class_='vr1PYe')
-        source = source_elem.text.strip() if source_elem else ''
-        
-        link_elem = article.find('a', class_='WwrzSb')
-        orglink = link_elem['href'] if link_elem else ''
-        link = re.sub(r'./read/', 'https://news.google.com/read/', orglink)
-        
-        article_data = {
-            'title': title,
-            'source': source,
-            'link': link,
-        }
-        
-        news_data.append(article_data)
-        
-    except Exception as e:
-        print(f"Error processing article: {e}")
-        continue
+for item in topiccwiz:
+    hotarticles = item.find_all("article")
+    first_article = ""
+    passed_first = False
+    for article in hotarticles:
+        try:
+            if not passed_first:
+                first_title_elem = article.find('a', class_='gPFEn')
+                first_title = first_title_elem.text.strip() if first_title_elem else ''
+                first_article = first_title
+                passed_first = True
+                continue
+            title_elem = article.find('a', class_='gPFEn')
+            title = title_elem.text.strip() if title_elem else ''
+            
+            source_elem = article.find('div', class_='vr1PYe')
+            source = source_elem.text.strip() if source_elem else ''
+            link_elem = article.find('a', class_='WwrzSb')
+            orglink = link_elem['href'] if link_elem else ''
+            link = re.sub(r'./read/', 'https://news.google.com/read/', orglink)
+            article_data = {
+                'title': title,
+                'source': source,
+                'link': link,
+                'category': first_article
+            }
+            news_data.append(article_data)
+        except Exception as e:
+            print(f"Error processing article: {e}")
+            continue
 
 with open('news.json', 'w', encoding='utf-8') as f:
     json.dump(news_data, f, ensure_ascii=False, indent=2)
