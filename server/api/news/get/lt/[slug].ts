0bcb646cc0362f76d23d879d4ec166863bc69864
吳元皓 <hw@yuanhau.com>
2025-05-18 11:02:32 +0800

    Archive the jssoup one for now

--- server/api/news/get/lt/[slug].ts
+++ server/api/news/get/lt/[slug].ts
@@ -1,20 +1,7 @@
+import lineToday from "~/server/scrape/line_today";
 export default defineEventHandler(async (event) => {
   const slug = getRouterParam(event, "slug");
-  return new Promise((resolve, reject) => {
-    const pythonProcess = spawn("python3", ["scraping/hot_articles.py"]);
-
-    let dataString = "";
-
-    pythonProcess.stdout.on("data", (data) => {
-      dataString += data.toString();
-    });
-
-    pythonProcess.stderr.on("data", (data) => {
-      console.error(`Error: ${data}`);
-    });
-
-    pythonProcess.on("close", (code) => {
-      resolve({ status: "completed", output: dataString });
-    });
-  });
+  const data = await lineToday(slug);
+  console.log(data);
+  return data;
 });
--- server/scrape/line_today.ts
+++ server/scrape/line_today.ts
@@ -1,4 +1,5 @@
 import JSSoup from "jssoup";
+//import cheerio from "cheerio";
 
 async function lineToday(slug: string) {
   const url = "https://today.line.me/tw/v2/article/" + slug;
@@ -16,8 +17,20 @@ async function lineToday(slug: string) {
       "Cache-Control": "max-age=0",
     },
   });
-  const data = fetchPageCode.text();
-  const soup = new JSSoup(data);
+  // 幹 又忘了 await
+  const data = await fetchPageCode.text();
+  // 加 await? no.
+  // AHHH I NEED TO CHANGE TO SOMETHING ELSE.
+  const soup = new JSSoup(data, false);
+  const titlesoup = soup.find("h1", "entityTitle");
+  const title = titlesoup.text.replaceAll("\n", "");
+
+  const article = soup.find("article", "news-content");
+  const paragraph = article.text;
+  return {
+    title: title,
+    paragraph: paragraph,
+  };
 }
 
 export default lineToday;
