a998a99901b5d308181acb47a143a425687bafbd
吳元皓 <hw@yuanhau.com>
2025-05-27 14:33:05 +0800

    Make a super simple shortforward.ts system for yhw.tw/news

---
+++ server/routes/shortforward.ts
@@ -0,0 +1,14 @@
+export default defineEventHandler((event) => {
+    const query = getQuery(event);
+    const toolCall = query.tool;
+    const forwardCall = query.forward;
+    if (toolCall) {
+        const buildUrl = "/desktop?openapp=" + toolCall;
+        return sendRedirect(event, buildUrl, 302);
+    }
+    if (forwardCall) {
+        const buildUrl = "/" + forwardCall;
+        return sendRedirect(event, buildUrl, 302);
+    }
+    return sendRedirect(event, "/", 302)
+})
