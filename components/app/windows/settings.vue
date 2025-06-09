6f759cb61292c1b86c951131d286541abf901d13
吳元皓 <hw@yuanhau.com>
2025-06-09 21:28:50 +0800

    Commit this just for the demo. & fix stuff.

--- components/app/windows/settings.vue
+++ components/app/windows/settings.vue
@@ -69,7 +69,7 @@ const submitCustomApiKey = async () => {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        value: customApiKey,
+        value: customApiKey.value,
       }),
     });
 
