026c4a31a3341723ffc9415aca990e25cc3485bd
吳元皓 <hw@yuanhau.com>
2025-06-14 23:31:24 +0800

    LoadInfo stuff & Fix validating stuff. (PLEASE DO THE STARRING FEAT!!!)

--- server/api/user/loadInfo.ts
+++ server/api/user/loadInfo.ts
@@ -1,7 +1,6 @@
-// Fixed data for testing
+/*// Fixed data for testing
 export default defineEventHandler(async (event) => {
   return {
-    langPref: "en",
     doNotShowLangPrefPopUp: false,
     email: "test@yuanhau.com",
     name: "Howard",
@@ -12,4 +11,50 @@ export default defineEventHandler(async (event) => {
       provider: "google", // Default provider
     },
   };
+});
+*/
+import sql from "~/server/components/postgres";
+import getUserTokenMinusSQLInjection from "~/server/components/getUserToken";
+export default defineEventHandler(async (event) => {
+  try {
+    const token = await getUserTokenMinusSQLInjection(event);
+    if (token.error.length !== 0) {
+      return {
+        error: token.error,
+      };
+    }
+    const fetchMainData = await sql`
+      SELECT * FROM users
+      WHERE username = ${token.user}
+      `;
+    const fetchOtherUserData = await sql`
+      SELECT * FROM user_other_data
+      WHERE username = ${token.user}
+      `;
+
+    if (fetchMainData.length === 0 || fetchOtherUserData.length === 0) {
+      return {
+        error: "ERR_USER_DOESNT_EXIST",
+      };
+    }
+    return {
+      doNotShowLangPrefPopUp:
+        fetchOtherUserData[0].remove_translate_popup || false,
+      email: fetchMainData[0].email || "",
+      name: fetchMainData[0].firstname || "",
+      useCustomGroqKey:
+        fetchOtherUserData[0].groq_api_key.length !== 0 || false,
+      translate: {
+        enabled: fetchOtherUserData[0].translate_enabled || false,
+        lang: "en",
+        provider: fetchOtherUserData[0].translate_provider || "google",
+      },
+    };
+  } catch (e) {
+    console.log(e);
+    return {
+      error: "ERR_SERVER_SIDE",
+      e: e.message,
+    };
+  }
 });
